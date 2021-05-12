f5d6105963c3fd61f525c9f328cfeea7ca5dbd59
Pat <stumbaup@oregonstate.edu>
2021-05-12 12:31:13 -0700

    testing out different image options

--- SERVICE/receiveURL.js
+++ SERVICE/receiveURL.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 var fs = require("fs");
 var sharp = require("sharp");
-const fetch = require("node-fetch");
 var amqp = require("amqplib/callback_api");
 var https = require("https");
 
@@ -35,10 +34,36 @@ function makeThumbnail() {
                 function (msg) {
                     console.log(" [x] Received %s", msg.content.toString());
 
-                    var url = msg.content.toString();
-                    path = "./urlImage.png";
+                    //get our image
+                    var imageToProcess = msg.content.toString();
+                    localPath = "./urlImage.png"; //where we are going to temporarily save the image
 
-                    saveImageToDisk(url, path);
+                    //find out the type of image and route it to the correct transformer service
+                    if (imageToProcess.includes("http") == true) {
+                        console.log(" # Image is a URL");
+                        saveImageFromURL(imageToProcess, localPath);
+                    } else if (
+                        //if the image is one of the acceptable sharp format files:
+                        imageToProcess.includes(".jpg") == true ||
+                        imageToProcess.includes(".jpeg") == true ||
+                        imageToProcess.includes(".png") == true ||
+                        imageToProcess.includes(".webp") == true ||
+                        imageToProcess.includes(".avif") == true ||
+                        imageToProcess.includes(".tiff") == true ||
+                        imageToProcess.includes(".gif") == true ||
+                        imageToProcess.includes(".svg") == true
+                    ) {
+                        console.log(" # Image is an acceptable picture file");
+                        saveImageAcceptablePic(imageToProcess, localPath);
+                    } else {
+                        //not a url or acceptable format, sending back a generic
+                        console.log(
+                            " # Image is not able to process, sending generic thumbnail"
+                        );
+                        saveImageGeneric(imageToProcess, localPath);
+                    }
+                    localPath = "";
+                    imageToProcess = "";
                 },
                 {
                     noAck: true,
@@ -48,20 +73,24 @@ function makeThumbnail() {
     });
 }
 
-//Node.js Function to save image from External URL.
-function saveImageToDisk(url, localPath) {
+//saves an image from a URL and sends it to queue
+function saveImageFromURL(url, localPath) {
+    //open a new fs write stream
     var file = fs.createWriteStream(localPath);
+    //pipe the file into response
+    //on close, resize the image and send it to the queue
     var request = https.get(url, function (response) {
         response.pipe(file);
         file.on("close", () =>
-            sharp("./urlImage.png")
-                .resize(200, 200)
+            sharp(localPath)
+                .resize(200, 200) //thumbnail size
                 .toBuffer()
                 .then((data) => {
-                    fs.writeFileSync("urlThumbnail.jpg", data);
-                    console.log("thumbnail created successfully!");
-                    sendThumbnailToQueue();
+                    fs.writeFileSync("thumbnail.jpg", data);
+                    console.log("URL - thumbnail created successfully!");
+                    sendThumbnailToQueue(); //send it to the queue
                 })
+                //error handle:
                 .catch((err) => {
                     console.log(err);
                 })
@@ -69,25 +98,52 @@ function saveImageToDisk(url, localPath) {
     });
 }
 
+//saves an "acceptable file format" for local images and sends it to queue
+function saveImageAcceptablePic(imageToProcess, localPath) {
+    sharp(imageToProcess)
+        .resize(200, 200) //thumbnail size
+        .toBuffer()
+        .then((data) => {
+            fs.writeFileSync("thumbnail.jpg", data);
+            console.log("Accp Image - thumbnail created successfully!");
+            sendThumbnailToQueue(); //send it to the queue
+        });
+}
+
+//saves a generic image and sends it to the rabbitMQ queue.
+function saveImageGeneric(url, localPath) {
+    sharp("./generic.jpg")
+        .resize(200, 200) //thumbnail size
+        .toBuffer()
+        .then((data) => {
+            fs.writeFileSync("thumbnail.jpg", data);
+            console.log("!! GENERIC !! thumbnail created successfully!");
+            //send it to the queue
+            sendThumbnailToQueue();
+        });
+}
+
+//takes the saved local thumbnail image ("urlThumbnail.jpg") and sends it to the rabbitmq queue
 function sendThumbnailToQueue() {
+    //connect to server
     amqp.connect(credentials.AMPQserver, function (error0, newConnection) {
         if (error0) {
             throw error0;
         }
+        //create a new channel
         newConnection.createChannel(function (error1, channel) {
             if (error1) {
                 throw error1;
             }
+            //setup our queue
             var queue = "getFromThumbnailQueue";
-
-            fs.readFile("./urlThumbnail.jpg", function (err, data) {
+            //read the file and send it to the queue
+            fs.readFile("./thumbnail.jpg", function (err, data) {
                 if (err) throw err; // Fail if the file can't be read.
-
                 channel.assertQueue(queue, {
                     durable: true,
                 });
-                channel.sendToQueue(queue, Buffer.from(data));
-
+                channel.sendToQueue(queue, Buffer.from(data)); //send it to queue
                 console.log(' [x] Sent image to "thumbnailReturn" queue');
                 return;
             });
