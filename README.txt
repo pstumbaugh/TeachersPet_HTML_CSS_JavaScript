88073f9ef0947ea05ec6665585fb2e7e3890eb01
Pat <stumbaup@oregonstate.edu>
2021-05-21 11:44:48 -0700

    update Service name and readme

--- README.txt
+++ README.txt
@@ -1,23 +1,27 @@
 Thumbnail transformer service:
 This service takes in an incoming image (URL or image type) as a message (through the "PatQueue" queue) through RabbitMQ. 
     It then transforms it into a thumbnail size (200x200) and sends the new image (saved as "thumbnail.jpg") as a message 
-    back through RabbitMQ (in the "thumbnailTransformer" exchange). 
+    back through RabbitMQ (in the "thumbnailTransformer" exchange). Be sure to be listening to that exchange before sending
+    any pictures for processing, as if the pics aren't picked up by a listening client, they will be discared immediately. 
+Basically ... send your pictures to the "PatQueue" queue. Listen to the "thumbnailTransformer" exchange to get the pictures 
+    back as thumbnails. 
 
 HOW THE SERVICE WORKS:
-(service is in the "SERVICE" folder in this repo)
-1. Once started, the service is always watching "PatQueue" for new messages
-2. Once a message is received from that queue, it is processed (transformed into a thumbnail, if possible)
+(service is in the "Service" folder in this repo)
+1. Once a message is received from that queue, it is processed (transformed into a thumbnail, if possible)
     *- If the message is a URL or acceptable picture type, the message will be transformed to a thumbnail
     *- If the message is not transformable via the service, a generic (leopard picture) will be used in it's place
-3. The new thumbnail will then be sent to the "thumbnailTransformer" exchange for pickup by a consumer.
-4. If you are the consumer as well (which is expeted in most cases), assert the "thumbnailTransformer" exchange and consume messages from there (in your own queue if needed)
+2. The new thumbnail will then be sent to the "thumbnailTransformer" exchange for pickup by a consumer.
+3. If you are the consumer as well (which is expeted in most cases), assert the "thumbnailTransformer" exchange and consume messages from there (in your own queue if needed)
     *- see example of consuming from the exchange in the file named "receiveTest.js" (see below for startup instructions)
+    *- be sure to only consume while you are waiting for your pictures. If you leave your consumer running, you may also get
+        pics from other users who use the service after you
 
 
-    
 STARTING THE SERVICE (and the test sending and receiving services too!):
-(Service will eventually be running forever on a flip server, but until then, feel free to follow the below instructions to test)
-1. Navigate into "SERVICE" folder
+    **Service is currently running "forever" on the OSU server, public_html folder, so you should be able to use it 
+(Below are instructions to test start the service, if needed)
+1. Navigate into "Service" folder
 2. run the thumbnail transofrmation service by entering in console: "node service.js"
     *- you may need to install node modules if needed
     *- The service will continue running until manually stopped. Please hit CTRL+C to stop service 
--- index.js
+++ index.js
@@ -1,42 +1,46 @@
-
 var goButton = document.getElementById("goButton");
+var testButton = document.getElementById("testButton");
 var numberOfResults = document.getElementById("ideaSize");
 var wikiURL = document.getElementById("wikiURL");
+
+function testButtonClick() {
+    console.log("HELLO!");
+}
 
 function goButtonClick() {
     var numberOfResultsValue = numberOfResults.value;
     var wikiURLValue = wikiURL.value;
 
     //error flags
-    wikiFlag = true
+    wikiFlag = true;
     ideasFlag = true;
 
     //check number of ideas entered correctly
     if (numberOfResultsValue <= 0) {
-        document.getElementById("ideaSizeError").textContent = "Please enter a positive number"
+        document.getElementById("ideaSizeError").textContent =
+            "Please enter a positive number";
         ideasFlag = true;
         event.preventDefault();
-    }
-    else if (numberOfResultsValue == '') {
-        document.getElementById("ideaSizeError").textContent = "Please enter a number"
+    } else if (numberOfResultsValue == "") {
+        document.getElementById("ideaSizeError").textContent =
+            "Please enter a number";
         ideasFlag = true;
         event.preventDefault();
-    }
-    else {
+    } else {
         document.getElementById("ideaSizeError").style.visibility = "hidden";
         ideasFlag = false;
         event.preventDefault();
     }
 
     //check wiki URL validity
-    if (wikiURLValue == '' || wikiURLValue.includes('wikipedia.org') == false) {
-        document.getElementById("wikiURLerror").textContent = "Please enter a wikipedia URL"
-        wikiFlag = true
+    if (wikiURLValue == "" || wikiURLValue.includes("wikipedia.org") == false) {
+        document.getElementById("wikiURLerror").textContent =
+            "Please enter a wikipedia URL";
+        wikiFlag = true;
         event.preventDefault();
-    }
-    else {
+    } else {
         document.getElementById("wikiURLerror").style.visibility = "hidden";
-        wikiFlag = false
+        wikiFlag = false;
         event.preventDefault();
     }
     event.preventDefault();
@@ -45,7 +49,7 @@ function goButtonClick() {
     if (ideasFlag == false && wikiFlag == false) {
         var url = "./results.html";
         window.location.replace(url);
-        console.log("Go button clicked")
+        console.log("Go button clicked");
     }
     event.preventDefault();
 }
@@ -54,17 +58,17 @@ function enterKeyCheck(ID) {
     //if user wants to hit the enter key while on the amount input field to submit:
     var wikiButtonClick = document.getElementById(ID);
     wikiButtonClick.addEventListener("keypress", function (event) {
-        if (event.keyCode === 13) // Number 13 is the "Enter" key on the keyboard
-        {
+        if (event.keyCode === 13) {
+            // Number 13 is the "Enter" key on the keyboard
             goButtonClick();
             event.preventDefault();
         }
     });
 }
 
-
 document.addEventListener("DOMContentLoaded", function () {
     goButton.addEventListener("click", goButtonClick);
     wikiURL.addEventListener("keyup", enterKeyCheck("wikiURL"));
     numberOfResults.addEventListener("keyup", enterKeyCheck("ideaSize"));
+    testButton.addEventListener("click", testButtonClick);
 });
